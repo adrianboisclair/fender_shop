e417c6f951fee3af2c8e340cc53f0e08cc4de019
Adrian Boisclair <adrianboisclair@gmail.com>
2016-12-12 20:20:37 -0800

    add header animations

--- assets/app/header/header.component.ts
+++ assets/app/header/header.component.ts
@@ -1,11 +1,12 @@
-import { Component } from "@angular/core";
+import { Component, HostListener, Inject, OnInit } from "@angular/core";
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
     selector: 'fender-header',
     styleUrls: ['./header.component.css'],
     templateUrl: './header.component.html'
 })
-export class FenderHeader {
+export class FenderHeader implements OnInit {
     brand:string = 'Fender';
     brand_image_url:string = '/images/fender_logo.png';
     links:any = [
@@ -14,5 +15,22 @@ export class FenderHeader {
         { name: 'Customize', url: '#custom' },
         { name: 'Dealers', url: '#dealers' }
     ];
+    navIsFixed:boolean = false;
     searchBoxText:string = 'search';
+
+    constructor(@Inject(DOCUMENT) private document: Document) { }
+
+    ngOnInit() { }
+
+    @HostListener("window:scroll", [])
+    onWindowScroll() {
+        let top = this.document.body.scrollTop;
+        console.log(top);
+        if (top > 64) {
+            this.navIsFixed = true;
+        } else if (this.navIsFixed && top < 64) {
+            this.navIsFixed = false;
+        }
+    }
+
 }
